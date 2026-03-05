a04d52c409f3bc66acca702cc502e4717a7401ec
Pavani <pavani.software26@gmail.com>
2026-03-05 16:34:40 -0600

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -11,9 +11,10 @@ AMI_ID="ami-0220d79f3f480ecf5"
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text)
-    if[$instance == "frontend"];then
+    if[$instance == "frontend"]; then
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' --output text)
     else
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PrivateIpAddress' --output text)
     fi
+    echo "IP Address: $IP"
   done
